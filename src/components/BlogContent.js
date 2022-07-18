7a0a9deffd188cc7e35bcc413541256688d671f6
mutemip <paulmutemi2030@gmail.com>
2022-07-18 13:35:55 +0300

    props basic

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+//distructuring props
+function BlogContent({isPublished, articleText, minutesToRead}) {
+  //console.log(props)
+  if (!isPublished){
+    return null;
+  }else{
+    return(
+      <div>
+        <h1>{articleText}</h1>
+        <p>{minutesToRead} minutes to read</p>
+      </div>
+    )
+  }
+  // return <div id="blog-content">{props.articleText}</div>;
 }
 
 export default BlogContent;
